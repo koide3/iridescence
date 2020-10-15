ea477c802456ed423edbf8a98df3c8dcb5fcf4d6
k.koide <k.koide@aist.go.jp>
2020-10-15 18:50:14 +0900

    update light_viewer_basic

--- src/example/light_viewer_basic.cpp
+++ src/example/light_viewer_basic.cpp
@@ -1,19 +1,20 @@
 #include <glk/primitives/primitives.hpp>
 #include <guik/viewer/light_viewer.hpp>
-#include <guik/recent_files.hpp>
 
 int main(int argc, char** argv) {
   auto viewer = guik::LightViewer::instance();
 
+  float scale = 1.0f;
   viewer->register_ui_callback("ui", [&]() {
-    ImGui::Text("Basic example");
+    ImGui::DragFloat("Scale", &scale, 0.01f, 0.01f, 10.0f);
+
     if(ImGui::Button("Close")) {
       viewer->close();
     }
   });
 
   while(viewer->spin_once()) {
-    Eigen::Matrix4f transform = (Eigen::AngleAxisf(ImGui::GetTime(), Eigen::Vector3f::UnitZ()) * Eigen::Isometry3f::Identity()).matrix();
+    Eigen::Matrix4f transform = (Eigen::AngleAxisf(ImGui::GetTime(), Eigen::Vector3f::UnitZ()) * Eigen::UniformScaling<float>(scale) * Eigen::Isometry3f::Identity()).matrix();
     viewer->update_drawable("icosahedron", glk::Primitives::primitive_ptr(glk::Primitives::CUBE), guik::Rainbow(transform));
   }
 
