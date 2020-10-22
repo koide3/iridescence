2c831bb364e4ab31b2d33908391b73acacd3a77f
k.koide <k.koide@aist.go.jp>
2020-10-22 19:49:10 +0900

    unified cmake/catkin build

---
+++ src/glk/path_std.cpp
@@ -0,0 +1,25 @@
+#include <glk/path.hpp>
+
+#include <iostream>
+// #include <ros/package.h>
+
+namespace glk {
+
+namespace {
+std::string data_path;
+}
+
+void set_data_path(const std::string& path) {
+  data_path = path;
+}
+
+std::string get_data_path() {
+  if(data_path.empty()) {
+    data_path = "./data";
+    // data_path = ros::package::getPath("gl_test_field") + "/data";
+  }
+
+  return data_path;
+}
+
+}  // namespace glk
