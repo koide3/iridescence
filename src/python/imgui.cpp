af5fc946d4d8f6c9fb94381a0cf6551dfb56c7e6
k.koide <k.koide@aist.go.jp>
2021-05-28 16:25:35 +0900

    add fancy buttons

--- src/python/imgui.cpp
+++ src/python/imgui.cpp
@@ -25,6 +25,12 @@ void define_imgui(py::module_& m) {
   imgui_.attr("Cond_FirstUserEver") = py::int_(static_cast<int>(ImGuiCond_FirstUseEver));
   imgui_.attr("Cond_Appearing") = py::int_(static_cast<int>(ImGuiCond_Appearing));
 
+  imgui_.attr("Dir_Left") = py::int_(static_cast<int>(ImGuiDir_Left));
+  imgui_.attr("Dir_Right") = py::int_(static_cast<int>(ImGuiDir_Right));
+  imgui_.attr("Dir_Up") = py::int_(static_cast<int>(ImGuiDir_Up));
+  imgui_.attr("Dir_Down") = py::int_(static_cast<int>(ImGuiDir_Down));
+  imgui_.attr("Dir_None") = py::int_(static_cast<int>(ImGuiDir_None));
+
   // functions
   imgui_.def("begin", [] (const std::string& name, bool open, int flags) { return std::make_tuple(ImGui::Begin(name.c_str(), &open, flags), open); });
   imgui_.def("end", [] { ImGui::End(); });
@@ -45,6 +51,8 @@ void define_imgui(py::module_& m) {
     }, "", py::arg("label"), py::arg("text"), py::arg("flags") = 0, py::arg("buffer_size") = 256
   );
   imgui_.def("button", [](const std::string& label) { return ImGui::Button(label.c_str()); });
+  imgui_.def("arrow_button", ImGui::ArrowButton);
+
   imgui_.def("checkbox", [](const std::string& label, bool v) { return std::make_tuple(ImGui::Checkbox(label.c_str(), &v), v); });
   imgui_.def("drag_int", [](const std::string& label, int v, int v_speed, int v_min, int v_max, const std::string& format)
     { return std::make_tuple(ImGui::DragInt(label.c_str(), &v, v_speed, v_min, v_max, format.c_str()), v); }, "",
@@ -54,6 +62,9 @@ void define_imgui(py::module_& m) {
     { return std::make_tuple(ImGui::DragFloat(label.c_str(), &v, v_speed, v_min, v_max, format.c_str(), power), v); }, "",
     py::arg("label"), py::arg("v"), py::arg("v_speed") = 1.0f, py::arg("v_min") = 0.0f, py::arg("v_max") = 0.0f, py::arg("format") = "%.3f", py::arg("power") = 1.0f
   );
+
+  imgui_.def("push_button_repeat", &ImGui::PushButtonRepeat);
+  imgui_.def("pop_button_repeat", &ImGui::PopButtonRepeat);
 
   // IO
   py::class_<ImGuiIO>(imgui_, "IO")
