eb043088dd10107d281da792c22476057cd657a4
k.koide <k.koide@aist.go.jp>
2023-01-03 19:42:59 +0900

    assimp-based loader

---
+++ include/glk/mesh_model.hpp
@@ -0,0 +1,33 @@
+#ifndef GLK_MESH_MODEL_HPP
+#define GLK_MESH_MODEL_HPP
+
+#include <glk/drawable.hpp>
+
+namespace guik {
+class ShaderSetting;
+}  // namespace guik
+
+namespace glk {
+
+class Mesh;
+class Texture;
+
+class MeshModel : public glk::Drawable {
+public:
+  void push_mesh(const int material_id, const std::shared_ptr<glk::Mesh>& mesh);
+  void push_material(const guik::ShaderSetting& setting, const std::shared_ptr<glk::Texture>& texture);
+  void override_material(const guik::ShaderSetting& setting, const std::shared_ptr<glk::Texture>& texture);
+
+  virtual void draw(glk::GLSLShader& shader) const override;
+
+private:
+  std::vector<int> material_ids;
+  std::vector<std::shared_ptr<glk::Mesh>> meshes;
+
+  std::vector<guik::ShaderSetting> settings;
+  std::vector<std::shared_ptr<glk::Texture>> textures;
+};
+
+}  // namespace glk
+
+#endif
