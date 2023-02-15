67303063ac52422c1c3e6d223d0975879b74b1f6
k.koide <k.koide@aist.go.jp>
2023-02-15 14:38:20 +0900

    refactor SSBO

--- include/glk/shader_storage_buffer.hpp
+++ include/glk/shader_storage_buffer.hpp
@@ -2,58 +2,29 @@
 #define GLK_SHADER_STORAGE_BUFFER_HPP
 
 #include <vector>
+#include <iostream>
 #include <GL/gl3w.h>
 
 namespace glk {
 
 class ShaderStorageBuffer {
 public:
-  ShaderStorageBuffer(size_t size, void* data = nullptr, GLenum usage = GL_DYNAMIC_COPY) {
-    this->size = size;
+  ShaderStorageBuffer(size_t size, void* data = nullptr, GLenum usage = GL_DYNAMIC_COPY);
+  ~ShaderStorageBuffer();
 
-    glGenBuffers(1, &ssbo);
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
-    glBufferData(GL_SHADER_STORAGE_BUFFER, size, data, usage);
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
-  }
+  size_t size() const;
 
-  ~ShaderStorageBuffer() {
-    glDeleteBuffers(1, &ssbo);
-  }
+  void set_data(size_t buffer_size, void* data);
+  void get_data(size_t buffer_size, void* data) const;
 
-  void reset(size_t size = 0, void* data = nullptr) {
-    if(size == 0) {
-      size = this->size;
-    }
-
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
-    if(data) {
-      glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, size, data);
-    } else {
-      std::vector<char> buffer(size, 0);
-      glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, size, buffer.data());
-    }
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
-  }
-
-  void get_values(void* data) const {
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
-    glGetBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, size, data);
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
-  }
-
-  void bind(int index = 0) {
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, index, ssbo);
-  }
-
-  void unbind(int index = 0) {
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, index, 0);
-  }
+  void bind(int index = 0);
+  void unbind(int index = 0);
 
 private:
-  size_t size;
+  size_t buffer_size;
   GLuint ssbo;
 };
+
 }  // namespace glk
 
 #endif
---
+++ src/glk/shader_storage_buffer.cpp
@@ -0,0 +1,59 @@
+#include <glk/shader_storage_buffer.hpp>
+
+#include <glk/console_colors.hpp>
+
+namespace glk {
+
+ShaderStorageBuffer::ShaderStorageBuffer(size_t size, void* data, GLenum usage) {
+  this->buffer_size = size;
+
+  glGenBuffers(1, &ssbo);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
+  glBufferData(GL_SHADER_STORAGE_BUFFER, size, data, usage);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+}
+
+ShaderStorageBuffer::~ShaderStorageBuffer() {
+  glDeleteBuffers(1, &ssbo);
+}
+
+size_t ShaderStorageBuffer::size() const {
+  return buffer_size;
+}
+
+void ShaderStorageBuffer::set_data(size_t buffer_size, void* data) {
+  if (buffer_size > this->buffer_size) {
+    std::cerr << console::yellow << "warning: specified input buffer size is larger than SSBO buffer size!!" << console::yellow << std::endl;
+    buffer_size = this->buffer_size;
+  }
+
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
+  if (data) {
+    glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, buffer_size, data);
+  } else {
+    std::vector<char> buffer(buffer_size, 0);
+    glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, buffer_size, buffer.data());
+  }
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+}
+
+void ShaderStorageBuffer::get_data(size_t buffer_size, void* data) const {
+  if (buffer_size > this->buffer_size) {
+    std::cerr << console::yellow << "warning: specified output buffer size is larger than SSBO buffer size!!" << console::yellow << std::endl;
+    buffer_size = this->buffer_size;
+  }
+
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
+  glGetBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, buffer_size, data);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+}
+
+void ShaderStorageBuffer::bind(int index) {
+  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, index, ssbo);
+}
+
+void ShaderStorageBuffer::unbind(int index) {
+  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, index, 0);
+}
+
+}  // namespace glk
