96e12b255ef6114dd60db3b1c054a32c730abaf1
koide3 <31344317+koide3@users.noreply.github.com>
2025-12-21 00:19:07 +0900

    add publish.yml (#182)
    * add publish.yml
    
    * fix python versions
    
    * install dependencies
    
    * recursive submodule clone
    
    * change ci name
    
    * publish
    
    * project info
    
    * fix licence
    
    * fix pyproject.toml
    
    * fix setup.py
    
    * publish only tags
    
    * fix pyridescence_data
    
    * fix
    
    * depedency on numpy
    
    * remove scipy

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 from setuptools import find_packages, setup
 setup(
-    name="pyridescence",
-    version="0.1.6",
-    author="k.koide",
-    author_email="k.koide@aist.go.jp",
-    description="A 3D visualization library for rapid proto-typing",
-    license="MIT",
     cmake_source_dir=".",
-    zip_safe=False,
     packages=['pyridescence_data'],
     package_dir={'pyridescence_data': 'data'},
-    python_requires=">=3.7",
-    include_package_data=True,
+    include_package_data=True
 )
