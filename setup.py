59aa0d10c2ed98fd5c470d0060fa8e0971f941ab
k.koide <k.koide@aist.go.jp>
2025-12-21 00:09:05 +0900

    fix

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import find_packages, setup
 setup(
     cmake_source_dir=".",
     packages=['pyridescence_data'],
-    package_dir={'pyridescence_data': 'data'}
+    package_dir={'pyridescence_data': 'data'},
+    include_package_data=True
 )
