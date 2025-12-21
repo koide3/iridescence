36fce0383ce68b83fe62fb51bb50691ba5110ff3
k.koide <k.koide@aist.go.jp>
2025-12-21 10:50:47 +0900

    specify image and fix license

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
+    name="pyridescence",
+    version="0.1.7",
+    author="Kenji Koide",
+    author_email="k.koide@aist.go.jp",
+    description="3D visualization library for rapid prototyping of 3D algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
     cmake_source_dir=".",
-    packages=['pyridescence_data'],
+    packages=['pyridescence_data', 'pyridescence_data.texture', 'pyridescence_data.shader', 'pyridescence_data.models'],
     package_dir={'pyridescence_data': 'data'},
-    include_package_data=True
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=["numpy"],
 )
