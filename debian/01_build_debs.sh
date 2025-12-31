7996d74dd468f1455c6319f77414d2367ef698f9
k.koide <k.koide@aist.go.jp>
2025-12-31 12:38:19 +0900

    add commit

--- debian/01_build_debs.sh
+++ debian/01_build_debs.sh
@@ -7,9 +7,13 @@ pushd .
 cd ..
 
 dch -v $1 $2 -D noble
+git add debian/changelog
+git commit -m "update debian/changelog for noble"
 gbp buildpackage -S -sa --git-upstream-tag='v%(version)s' --git-submodules
 
 dch -v $1 $2 -D jammy
+git add debian/changelog
+git commit -m "update debian/changelog for jammy"
 gbp buildpackage -S -sa --git-upstream-tag='v%(version)s' --git-submodules
 
 popd
