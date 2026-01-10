f18a5bc879fbdb0780227858b35514f618c13b92
k.koide <k.koide@aist.go.jp>
2026-01-11 05:37:33 +0900

    1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyridescence",
-    version="1.0.0",
+    version="1.0.1",
     author="Kenji Koide",
     author_email="k.koide@aist.go.jp",
     description="3D visualization library for rapid prototyping of 3D algorithms",
