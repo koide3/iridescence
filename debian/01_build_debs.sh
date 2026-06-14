00a81b6d4288a7fe1916e63ac28fe2b79059fa06
k.koide <k.koide@aist.go.jp>
2026-06-14 19:26:18 +0900

    v1.0.2

--- debian/01_build_debs.sh
+++ debian/01_build_debs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+VERSION=1.0.2
+MESSAGE="backface culling"
+
 export DEBFULLNAME="Kenji Koide"
 export DEBEMAIL="k.koide@aist.go.jp"
 
@@ -6,19 +9,21 @@ pushd .
 
 cd ..
 
-dch -v $1 $2 -D resolute
-git add debian/changelog
-git commit -m "update debian/changelog for resolute"
-gbp buildpackage -S -sa --git-upstream-tag='v%(version)s' --git-submodules
-
-dch -v $1 $2 -D noble
-git add debian/changelog
-git commit -m "update debian/changelog for noble"
-gbp buildpackage -S -sa --git-upstream-tag='v%(version)s' --git-submodules
-
-dch -v $1 $2 -D jammy
+dch -v $VERSION-1ppa1~jammy1 "$MESSAGE" -D jammy
 git add debian/changelog
 git commit -m "update debian/changelog for jammy"
 gbp buildpackage -S -sa --git-upstream-tag='v%(version)s' --git-submodules
 
+dch -v $VERSION-1ppa1~noble1 "$MESSAGE" -D noble
+git add debian/changelog
+git commit -m "update debian/changelog for noble"
+gbp buildpackage -S -sa --git-upstream-tag='v%(version)s' --git-submodules
+
+dch -v $VERSION-1ppa1~resolute1 "$MESSAGE" -D resolute
+git add debian/changelog
+git commit -m "update debian/changelog for resolute"
+gbp buildpackage -S -sa --git-upstream-tag='v%(version)s' --git-submodules
+
 popd
+
+echo "Please run 'dput ppa:koide3/iridescence ../*.changes' to upload the package to the PPA."
